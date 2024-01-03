944666f0f3d5e180f9fa0afc592749f15dd22d91
LUFEC4T0 <luigi02190622@gmail.com>
2024-01-03 00:44:22 -0500

    Se completaron los ejercicios de DOM

--- index.js
+++ index.js
@@ -14,16 +14,31 @@ const nameFooter = " Luigi Carrascal Torres cohort #53"
 
 $footerP.textContent += nameFooter
 
-const $main = document.querySelector('main')
-$main.innerHTML += '<div id="contenedor"> </div>'
-
 const $divId = document.getElementById('contenedor')
 
-function cardMaker () {
-    const card = document.createElement('article')
-    card.innerHTML += `<h3></h3>
-    <img></img>
-    <p></p>`
-    return card;
+function cardMaker(frutas) {
+    return `<section class="flex flex-col items-center w-96 border rounded border-black text-center">
+    <img class="w-64 h-48 object-cover" src="${frutas.foto}" alt="Foto fruta">
+    <h3 class="">${frutas.nombre}</h3>
+    <p class="">${frutas.descripcion}</p>
+</section>`
 }
 
+$divId.innerHTML += frutas.reduce (
+    (acumulador, fruta) => (acumulador += cardMaker(fruta)),
+    " "
+)
+
+const frutasDulces = frutas.filter((fruta) => fruta.esDulce);
+function dulces(elementos) {
+  const listaUl = document.createElement(`ul`);
+  elementos.forEach((fruta) => {
+    const listaLi = document.createElement(`li`);
+    listaLi.textContent = fruta.nombre;
+    listaUl.appendChild(listaLi);
+  });
+  return listaUl;
+}
+
+const $lista = document.getElementById(`lista`);
+$lista.appendChild(dulces(frutasDulces));
